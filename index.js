069dca064ecb0751c392bfe2fd808769f9d6d5ec
rishan3 <muhammedrishank0@gmail.com>
2023-01-12 10:29:08 +0530

    button

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const btn= document.querySelector(".container");
 const line=document.getElementsByClassName("line")
 btn.addEventListener('click',()=>{
-  line[0].classList.toggle("line1");
-  line[2].classList.toggle("line3");
-  line[1].classList.toggle("line2");
+    line[0].classList.toggle("line1");
+    line[2].classList.toggle("line3");
+    line[1].classList.toggle("line2");
 })
